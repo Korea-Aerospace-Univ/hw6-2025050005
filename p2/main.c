cf789e66946c4bda4df2ed8655ac45984efc0b13
2025050005 <inhyang227@gmail.com>
2026-05-27 16:00:32 +0900

    HW6-2.c

---
+++ p2/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int a[20], b[20];
+    int *p, *q;
+    
+    scanf("%d", &n);
+    
+    for (int i=0; i<n; i++){
+        scanf("%d", &a[i]);
+    }
+    
+    for (int i=0; i<n; i++){
+        scanf("%d", &b[i]);
+    }
+    
+    p=a+(n-1);
+    q=b+(n-1);
+    
+    for (int i=0; i<n; i++){
+        printf(" %d", *p + *q);
+        p--;
+        q--;
+    }
+   
+    return 0;
+}
